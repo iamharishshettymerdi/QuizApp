a4e6ae0ab1e21161e537ea9f4b2776e9614ec7e0
iamharishshettymerdi <harisha@litmus7.com>
2022-05-20 13:09:10 +0530

    commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+var HtmlWebpackPlugin=require("html-webpack-plugin")
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
